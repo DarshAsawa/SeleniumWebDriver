7de7e0e267aedeb2a81c89288e28c88d216c2038
Darsh Asawa <darshasawa@yahoo.co.in>
2020-03-27 23:37:02 +0530

    Added search functionality in firefox.java

--- amazon/src/amazon/Firefoxdriver.java
+++ amazon/src/amazon/Firefoxdriver.java
@@ -1,6 +1,9 @@
 package amazon;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -9,21 +12,32 @@ import org.testng.annotations.Test;
 public class Firefoxdriver {
 	
 	public static WebDriver driver;
-	public String email;
-	public String password;
+	public String baseURL="https://www.amazon.in";
 	
 
 @BeforeTest
 public void beforetest() {
+	
+	//system property needs modification according to your file paths, as it is responsible for execution and handling of web drivers.
 	System.setProperty("webdriver.gecko.driver", "M:\\WebTesting\\firefoxdriver\\geckodriver.exe");
 
 	driver=new FirefoxDriver();
-	driver.manage().window().maximize();
+	driver.manage().window().maximize();//to maximize the window.	
 	
+	driver.get(baseURL);//to open amazon in the browser.
 }
 
+//Search functionality..
 @Test
-public void login() {
+public void search() throws InterruptedException {
+	
+	WebElement element1 = driver.findElement(By.id("twotabsearchtextbox"));
+	element1.sendKeys("Dell");
+	element1.sendKeys(Keys.ENTER);
+	Thread.sleep(2000);
+	WebElement element2 = driver.findElement(By.partialLinkText("Core i5 8th gen"));
+	element2.click();
+	
 	
 }
 
