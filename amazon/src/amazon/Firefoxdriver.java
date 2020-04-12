bd29e90f3f8e59a6efc3839c9fa627ccbda5272e
Darsh Asawa <darshasawa@yahoo.co.in>
2020-04-12 19:53:01 +0530

    added more annotations like BeforeMethod and AfterMethod in firefox code.

--- amazon/src/amazon/Firefoxdriver.java
+++ amazon/src/amazon/Firefoxdriver.java
@@ -1,10 +1,13 @@
 package amazon;
 
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -14,18 +17,26 @@ public class Firefoxdriver {
 	
 	public static WebDriver driver;
 	public String baseURL="https://www.amazon.in";
+	public String searchelement = null;
+	Scanner scan;
 	
 
 @BeforeTest
 public void beforetest() {
-	
 	//system property needs modification according to your file paths, as it is responsible for execution and handling of web drivers.
 	System.setProperty("webdriver.gecko.driver", "M:\\WebTesting\\firefoxdriver\\geckodriver.exe");
-
 	driver=new FirefoxDriver();
+	driver.get(baseURL);
 	driver.manage().window().maximize();//to maximize the window.	
 	
 	
+}
+@BeforeMethod
+public void searchElement() {
+	//Method for asking user for search element before every test case
+	scan = new Scanner(System.in);
+	System.out.println("Enter the product name for searching : ");
+	searchelement = scan.nextLine();
 }
 
 //Search functionality..
@@ -33,16 +44,20 @@ public void beforetest() {
 public void search() throws InterruptedException {
 	//Searching for dell using search bar.
 	WebElement element1 = driver.findElement(By.id("twotabsearchtextbox"));
-	element1.sendKeys("Dell");
+	element1.sendKeys(searchelement);
 	element1.sendKeys(Keys.ENTER);
 	
 	Thread.sleep(2000);//letting the page load properly in case of slow connections.
 	
-	//using a partial text in order to search for core i5 8th Gen dell laptops..
-	WebElement element2 = driver.findElement(By.partialLinkText("Core i5 8th gen"));
+	//Only asking for prime supported items only ...
+	WebElement element2 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/div[3]/span/div/div[1]/ul/li/span/a/i"));
 	element2.click();
-	
-	
+		
+}
+
+@AfterMethod 
+public void afterSearch() {
+	scan.close();
 }
 
 @AfterTest
